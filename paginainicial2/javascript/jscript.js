8b7127afcf82fe2c047c608871804e9c13c31dd0
belani <otavio_belani@hotmail.com>
2014-06-10 01:34:50 -0300

    Update jscript.js

--- paginainicial2/javascript/jscript.js
+++ paginainicial2/javascript/jscript.js
@@ -8,16 +8,4 @@
 					}
 				
 
- function novaJanelaTec(pagina,nome,w,h,scroll){
-						leftPosition = (screen.width) ? (screen.width-w)/2 : 0;
-						topPosition = (screen.height) ? (screen.height-h)/2 : 0;
-						settings = 'height=' +h+ ',width=' +w+ ",top=" +topPosition+', left='+leftPosition+',scrollbars='+',resizable'
-						win = window.open(pagina,nome,settings);
-					}				
-
-function novaJanelaRelAtleta(pagina,nome,w,h,scroll){
-						leftPosition = (screen.width) ? (screen.width-w)/2 : 0;
-						topPosition = (screen.height) ? (screen.height-h)/2 : 0;
-						settings = 'height=' +h+ ',width=' +w+ ",top=" +topPosition+', left='+leftPosition+',scrollbars='+',resizable'
-						win = window.open(pagina,nome,settings);
-					}									
+	
